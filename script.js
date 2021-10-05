445568830378444e71ba7bc2e06267e90e50527c
Sagar Raj Gantayat <78720488+sagarcode28@users.noreply.github.com>
2021-10-05 19:21:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+      // smooth scrolling
+  
+      $('a[href*="#"]').on('click',function(e){
+  
+        e.preventDefault();
+  
+        $('html, body').animate({
+  
+          scrollTop : $($(this).attr('href')).offset().top,
+  
+        },
+        500,
+        'linear'
+        )
+  
+      });
